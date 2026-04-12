4075db68b6012ec9a475445d98b16fa4311b495e
hepan <916145267@qq.com>
2026-04-12 15:28:03 +0800

    api_router警告

--- base/plugins/thirdparty/api/v1/__init__.py
+++ base/plugins/thirdparty/api/v1/__init__.py
@@ -2,8 +2,8 @@
 第三方平台API路由
 """
 from fastapi import APIRouter
-from .platform import router as platform_router
-from .agent import router as agent_router
+from .platform import platform_router
+from .agent import agent_router
 
 api_router = APIRouter(prefix="/thirdparty")
 api_router.include_router(platform_router)
