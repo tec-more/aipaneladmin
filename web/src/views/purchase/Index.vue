485474dd6e22ad5891a255c1b77608fdfbc07afc
hepan <916145267@qq.com>
2026-07-01 22:22:59 +0800

    添加采购模块

---
+++ web/src/views/purchase/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="purchase-index">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.purchase-index {
+  height: 100%;
+}
+</style>
