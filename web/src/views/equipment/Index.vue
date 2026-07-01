de3a3b5d0f7b9b3b896fcc21fe84252c444c4acb
hepan <916145267@qq.com>
2026-07-01 19:23:17 +0800

    添加质量模块和设备模块的前端渲染组件

---
+++ web/src/views/equipment/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="equipment-index">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.equipment-index {
+  height: 100%;
+}
+</style>
---
+++ web/src/views/quality/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="quality-index">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.quality-index {
+  height: 100%;
+}
+</style>
